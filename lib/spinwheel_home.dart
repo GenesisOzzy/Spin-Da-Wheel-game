dc0511098913671f902f467870471975b27ff392
Grace Oliver <grace@proconnectpay.com>
2024-01-22 23:03:48 +0000

    import math library to use Random

--- lib/spinwheel_home.dart
+++ lib/spinwheel_home.dart
@@ -1,7 +1,9 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_fortune_wheel/flutter_fortune_wheel.dart';
 import 'package:rxdart/rxdart.dart';
-import 'dart:math';
+
 
 class SpinWheelHome extends StatefulWidget {
   const SpinWheelHome({Key? key}) : super(key: key);
@@ -12,7 +14,6 @@ class SpinWheelHome extends StatefulWidget {
 
 class _SpinWheelHomeState extends State<SpinWheelHome> {
   final selected = BehaviorSubject<int>();
-  int rewards = 0;
   int spinCount = 0;
 
   List<dynamic> items = [
@@ -56,8 +57,7 @@ class _SpinWheelHomeState extends State<SpinWheelHome> {
   }
 
   Widget buildFortuneWheel() {
-    return SizedBox(
-      height: 300,
+    return SizedBox.expand(
       child: FortuneWheel(
         selected: selected.stream,
         animateFirst: false,
@@ -67,59 +67,59 @@ class _SpinWheelHomeState extends State<SpinWheelHome> {
         ],
         onAnimationEnd: () {
           setState(() {
-            rewards = items[selected.value] is int ? items[selected.value] : 0;
             spinCount++;
 
-            // Check if the user has spun 10 times
             if (spinCount % 10 == 0) {
-              rewards += 10;
               showSnackBar("Congratulations! You just won an additional 10 Cedis!");
             }
 
-            // Check if the user has spun 20 times
             if (spinCount % 20 == 0) {
-              rewards += 15;
               showSnackBar("Amazing! You just won an additional 15 Cedis!");
             }
           });
 
-          showSnackBar("You just won $rewards Cedis!");
+          showSnackBar("You just won ${items[selected.value]} Cedis!");
         },
       ),
     );
   }
 
   Widget buildSpinButton() {
-    return GestureDetector(
-      onTap: () {
-        setState(() {
-          int selectedIndex;
-
-          if (spinCount == 9) { // Check for the tenth spin
-            if (Random().nextDouble() < 0.4) { // 40% probability for highest value
-              selectedIndex = items.indexOf(11); // Index of the highest value
-            } else {
-              List<int> shuffledItems = items
-                .map((item) => item is int ? items.indexOf(item) : -1)
-                .toList()
-                ..removeWhere((index) => index == -1);
-              selectedIndex = shuffledItems.isNotEmpty
-                ? Random().nextInt(shuffledItems.length)
-                : 0; // Default to 0 if only text prizes are left
-            }
-          } else {
-            List<int> shuffledItems = items
-              .map((item) => item is int ? items.indexOf(item) : -1)
-              .toList()
-              ..removeWhere((index) => index == -1);
-            selectedIndex = shuffledItems.isNotEmpty
-              ? Random().nextInt(shuffledItems.length)
-              : 0; // Default to 0 if only text prizes are left
-          }
-          selected.add(selectedIndex);
-        });
+    return ElevatedButton(
+      onPressed: () {
+        spinWheel();
       },
+      child: Text('Spin'),
     );
+  }
+
+  void spinWheel() {
+    setState(() {
+      int selectedIndex;
+
+      if (spinCount == 9) {
+        if (Random().nextDouble() < 0.4) {
+          selectedIndex = items.indexOf(11);
+        } else {
+          List<int> shuffledItems = items
+            .map((item) => item is int ? items.indexOf(item) : -1)
+            .toList()
+              ..removeWhere((index) => index == -1);
+          selectedIndex = shuffledItems.isNotEmpty
+              ? Random().nextInt(shuffledItems.length)
+              : 0;
+        }
+      } else {
+        List<int> shuffledItems = items
+          .map((item) => item is int ? items.indexOf(item) : -1)
+          .toList()
+            ..removeWhere((index) => index == -1);
+        selectedIndex = shuffledItems.isNotEmpty
+            ? Random().nextInt(shuffledItems.length)
+            : 0;
+      }
+      selected.add(selectedIndex);
+    });
   }
 
   Widget buildBottomNavigationBar() {
@@ -151,7 +151,7 @@ class _SpinWheelHomeState extends State<SpinWheelHome> {
     );
   }
 
- void showSnackBar(String message) {
+  void showSnackBar(String message) {
     ScaffoldMessenger.of(context).showSnackBar(
       SnackBar(
         content: Text(message),
